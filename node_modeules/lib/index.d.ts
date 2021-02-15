bf833129c294739bc497427e398ab93d29abff10
Snnaples <68153702+Snnaples@users.noreply.github.com>
2021-02-15 15:56:35 +0200

    Delete node_modeules/lib directory

--- node_modeules/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * as IdGenerator from './IdGenerator';
-export * as VrpTunnel from './VrpTunnel';
-export * as VrpProxy from './VrpProxy';
--- node_modeules/lib/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VrpProxy = exports.VrpTunnel = exports.IdGenerator = void 0;
-exports.IdGenerator = require("./IdGenerator");
-exports.VrpTunnel = require("./VrpTunnel");
-exports.VrpProxy = require("./VrpProxy");
